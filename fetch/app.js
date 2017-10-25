76353d275d3381100e0732802164f7841d56a4a9
Ivo Stoyanov <ivostoy@gmail.com>
2017-10-24 22:08:05 -0700

    wip

--- fetch/app.js
+++ fetch/app.js
@@ -1,7 +1,27 @@
-import { fetch } from 'node-fetch';
+import fetch from "node-fetch";
+fetch("https://www.google.com")
+    .then((res) => {
+    console.log(res.ok);
+    console.log(res.status);
+    console.log(res.statusText);
+    // console.log(res.headers);
+    console.log(res.headers.get("content-type"));
+})
+    .catch((err) => {
+    console.error(err);
+});
 fetch("https://api.github.com/users/github")
     .then((res) => {
     return res.json();
 }).then((json) => {
     console.log(json);
+});
+fetch("http://httpbin.org/post", { method: "POST", body: "foo=bar" })
+    .then((res) => {
+    return res.json();
+}).then((json) => {
+    console.log(json);
+})
+    .catch((err) => {
+    console.error(err);
 });
