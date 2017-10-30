6561c29ba2ff0c1d06c3169a910c91d79831f002
Ivo Stoyanov <ivostoy@gmail.com>
2017-10-30 00:03:27 -0700

    signin

--- crawler/app.js
+++ crawler/app.js
@@ -1,11 +1,93 @@
 /*
 https://github.com/cheeriojs/cheerio#-selector-context-root-
 https://github.com/gahabeen/jsonframe-cheerio
+https://github.com/rchipka/node-osmosis
+https://github.com/rchipka/node-osmosis/wiki
+
+http://blog.system-mining.xyz/web-scraper-for-nodejs-node-osmosis/
+
+osmosis  
+.get('https://www.facebook.com/login')
+.submit("#login_form", 
+{
+   'email': '--------@gmail.com',
+   'pass':'**********'
+})
+.then(function(context, data) {
+    osmosis.config('cookies', context.cookies)
+    osmosis.headers(context.request.headers)
+})
 */
+
+const osmosis = require('osmosis');
 const request = require('request');
 const cheerio = require('cheerio');
 const jsonframe = require('jsonframe-cheerio');
 const url = `http://money.cnn.com/data/premarket/`;
+const sign_in = `https://account.technitraderonlinecampus.com/sign-in/`;
+const user_agent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/60.0`
+var options = {
+  url: sign_in,
+  headers: {
+    'User-Agent': user_agent
+  }
+};
+
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(response.headers['set-cookie']);
+    // console.log(body);
+  }
+}
+
+request(options, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  } 
+  if (response.statusCode !== 200) {
+    console.error(response.statusCode);
+    return;
+  }
+  console.log(response.headers['set-cookie']);
+  console.log(body);
+  
+});
+
+// osmosis
+// .get(signin)
+
+// .find('#signin')
+// .then((ctx) => { 
+//   console.log(ctx); 
+// })
+// .set({
+//     'foo': '#signin'
+// })
+//.set('location')
+//.follow('@href')
+// .find('header + div + div li > a')
+// .set('category')
+// .follow('@href')
+// .paginate('.totallink + a.button.next:first')
+// .find('p > a')
+// .follow('@href')
+// .set({
+//     'title':        'title',
+//     'images':       ['img@src']
+// })
+// get data of all value in set
+
+// .data(function(data) {
+//   console.log('===========')
+//   console.log(data)
+//   console.log('===========')
+// })
+// .log(console.log)
+// .error(console.log)
+// .debug(console.log)
+
+/*
 
 const contents = `<!DOCTYPE html>
 <html lang="en">
@@ -61,11 +143,9 @@ function parse(html) {
     }	
   };
 
-  /*
 let frame2 = {
   "names": ["#pricing .item .planName"]
 };
-*/
 
   let frame3 = {
     "email": "[itemprop=email] < phone",
@@ -81,6 +161,7 @@ let frame2 = {
 }
 
 parse(contents);
+*/
 
 /*
 request(url, (err, resp, html) => {
