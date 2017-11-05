c1ba14080dbd38dbade96346866046354fdb71f5
Ivo Stoyanov <ivostoy@gmail.com>
2017-11-04 19:22:22 -0700

    dont use json-frame 3 - use 2.0.5

---
+++ crawler/src/t.js
@@ -0,0 +1,62 @@
+let cheerio = require('cheerio');
+let _ = require('lodash')
+const jsonframe = require('jsonframe-cheerio');
+//let jsonframe = require('./index.js')
+
+let html = `
+<html>
+<head></head>
+<body>
+    <h2>Pricing</h2>
+		<img class="picture" src="somepath/to/image.png">
+		<a class="mainLink" href="some/url/to/somewhere">A Link</a>
+		<span class="date"> We are the 04/02/2017</span>
+		<div class="popup"><span>Some inner content</span></div>
+    <ul id="pricing" class="menu">
+        <li class="item">
+            <span class="planName">Hacker</span>
+            <span class="planPrice" price="0">Free</span>
+            <a href="/hacker"> <img src="./img/hacker.png"> </a>
+        </div>
+        <li class="item">
+            <span class="planName">Pro</span>
+            <span class="planPrice" price="39.00">$39</span>
+            <a href="/pro"> <img src="./img/pro.png"> </a>
+        </div>
+    </ul>
+	<div id="contact">
+		<span itemprop="usaphone">Phone USA: (912) 148-456</div>
+		<span itemprop="frphone">Phone FR: +332 38 30 37 90</div>
+		<span itemprop="email">Email: lspurcell@suddenlink.net</div>
+	</div>
+</body>
+</html>
+`
+let $ = cheerio.load(html)
+
+jsonframe($)
+
+			let frame = {
+				"proPrice": {
+					_s: ".planName:contains('Pro') + span",
+					_a: "price"
+				}
+			}
+
+			let output = $('body').scrape(frame)
+      		console.log(output)
+
+			// let frame = {
+			// 	"pricing": {
+			// 		_s: "#pricing .item",
+			// 		_d: [{
+			// 			"name": ".planName",
+			// 			"price": ".planPrice @ price",
+			// 			"image": {
+			// 				"url": "img",
+			// 				"link": "a @ href"
+			// 			}
+			// 		}]
+			// 	}
+			// }
+
