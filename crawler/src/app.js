dcfa07f93dcfbecf7445b64f713b47236b4246c7
Ivo Stoyanov <ivostoy@gmail.com>
2017-11-05 12:17:24 -0800

    wip

--- crawler/src/app.js
+++ crawler/src/app.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 const Horseman = require('node-horseman')
 const co = require('co');
 const mkdirp = require('mkdirp');
-
+var prefix = '';
 /*
 https://www.npmjs.com/package/node-horseman
 https://github.com/johntitus/node-horseman
@@ -17,8 +17,8 @@ https://wiki.technitraderonlinecampus.com/services:market:2017-11-02
 const UA = 'Mozilla/5.0 Firefox/27.0';
 const HOST = 'https://wiki.technitraderonlinecampus.com';
 const LOGIN = `https://account.technitraderonlinecampus.com/sign-in`;
-const DME = `${HOST}/services:dme:start`;
-// const DME2 = `https://wiki.technitraderonlinecampus.com/services:market:2017-11-02`;
+//const DME = `${HOST}/services:dme:start`;
+const DME2 = `https://wiki.technitraderonlinecampus.com/services:market:2017-11-02`;
 // const SERV = `https://wiki.technitraderonlinecampus.com/services:`;
 const EMAIL = 'ivostoy@gmail.com';
 const PASS = 'Svetlana1`';
@@ -29,7 +29,6 @@ function parseList(html) {
   let $ = cheerio.load(html)
   const sel = 'div.level2';
   let res2 = $(sel);
-  // console.log(res2.length + ' children of ' + sel);
 
   res2.each(function (i, el) {
     // let x = $(this).html()
@@ -43,34 +42,48 @@ function parseList(html) {
 
 function scrape(h, el) {
   console.log(el);
-  const name = el.text;
+  const name = `${prefix}-${el.text}`;
   const dir = ensureDir(name);
   const url = HOST + el.q;
-  const path = `$dir/${name}.pdf`;
+  const path = `${dir}/${name}.pdf`;
   console.log('url ' + url + ', path ' + path);
-  return h.open()
+  return h.open(url)
     .wait(1000)
+    //.pdf(path);
     .pdf(path, {
       format: 'Letter',
-      orientation: 'landscape',
-      margin: '0.2in',
+      orientation: 'portrait',
+      margin: '0.5in',
       header: {
-        height: '0.1in',
-        contents: function (pageNum, numPages) {
-          return `<h3> ${name}         ${pageNum}/${numPages}</h3>`;
+        height: '3cm',
+        contents: function(pageNum, numPages) {
+          if (pageNum == 1) {
+            return '';
+          }
+          return '<h3>Header ' + pageNum + ' / ' + numPages + '</h3>';
+        }
+      },
+      footer: {
+        height: '3cm',
+        contents: function(pageNum, numPages) {
+          if (pageNum == 1) {
+            return '';
+          }
+          return '<h3>Footer ' + pageNum + ' / ' + numPages + '</h3>';
         }
       }
-    })
+        });
 }
 
 function ensureDir(date) {
+	console.log('ensureDir', date);	
 	const t = date.split('-');
-	const yr = t[0];
-	const mon = t[1];
-	const day = t[2];
-	console.log('ensureDir', yr, mon, day);	
+	const yr = t[1];
+	const mon = t[2];
+	// const day = t[3];
+	//console.log('ensureDir', yr, mon);	
 	const path = `./out/${yr}/${mon}`;
-	console.log(path);
+	console.log('ensureDir', path);
 	mkdirp(path, function(err) { 
 		if (err) {
       console.log(err);
@@ -94,15 +107,28 @@ co(function* () {
     .waitForNextPage()
     ;
   // get list of articles
-  yield horseman.open(DME)
+  /*
+  prefix = 'dme';
+  prefix = 'market';
+  prefix = 'ptw';
+  //fixme
+  prefix = 'ltpd';
+  */
+  prefix = 'ltpd';
+  prefix = 'dme';
+  
+  const URL = `${HOST}/services:${prefix}:start`;
+
+  yield horseman.open(URL)
   const html = yield horseman.html();
   const list = parseList(html);
-  console.log("got " + list.length + ' articles');
+  console.log("got " + list.length + ' ' + prefix + ' articles');
   yield scrape(horseman, list[0]);
-  yield scrape(horseman, list[100]);
-  yield scrape(horseman, list[400]);
+  // yield scrape(horseman, list[100]);
+  // yield scrape(horseman, list[400]);
   // for (var el in list) {
-  //   scrape(h, el);
+  //   yield horseman.wait(2000);
+  //   scrape(horseman, el);
   // }
   yield horseman.close();
 }).catch(function (e) {
