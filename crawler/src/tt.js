a94a3f7e8004192afa4329ce87a93c4c331086ae
Ivo Stoyanov <ivostoy@gmail.com>
2017-11-05 10:05:01 -0800

    ENOUGH

--- crawler/src/tt.js
+++ crawler/src/tt.js
@@ -2,7 +2,7 @@ let cheerio = require('cheerio');
 let _ = require('lodash')
 const jsonframe = require('jsonframe-cheerio');
 //let jsonframe = require('./index.js')
-
+// https://www.gitbook.com/book/kevinchisholm/basic-web-scraping-with-node-js-and-cheerio-js/details
 let html = `
 <html>
 <body>
@@ -23,9 +23,7 @@ let html = `
 	</h2>
 	<div class="level2">
 		<ul>
-			<li class="level1"><div class="li"> <a href="/services:dme:2017-01-01" class="wikilink1" title="services:dme:2016-01-29">2016-01-29</a></div>
-			</li>
-			<li class="level1"><div class="li"> <a href="/services:dme:2017-01-02" class="wikilink1" title="services:dme:2016-01-28">2016-01-28</a></div>
+			<li class="level1"><div class="li"> <a href="/services:dme:2017-01-02" class="wikilink1" title="services:dme:2017-01-02">2017-01-02</a></div>
 			</li>
 		</ul>
 	</div>
@@ -45,27 +43,59 @@ let frame1 = {
 }
 let frame2 = {
 	tt: {
-		_s: "div.level2 ul",
-		_d: [{
-			aa: {
-				_s: "div.li",
-				_d: [{
-					day: "a@href",
-				}]
-			}
-		}]
+		_s: "a.wikilink1",
+		_d: {
+			link: "@href",
+		}
 	}
 }
 
-let res1 = $('body').scrape(frame1)
+// let res1 = $('body').scrape(frame1)
 // console.log(res1)
 
-let res2 = $('body').scrape(frame2)
+// let res2 = $('body').scrape(frame2)
+// console.log(res2)
+
+let sel = 'h2.sectionedit24'; // 
+sel = 'div.level2';
+let res2 = $(sel);
+console.log(res2.length + ' children of ' + sel);
+
+res2.each(function(i, el) {
+	// let x = $(this).html()
+	let x = $(this).find('a.wikilink1');
+	console.log(x.length + ' children of a.wikilink1');
+	// console.log(x);
+	x.each(function(i, el) {
+		const text = $(this).text();
+		const href = el.attribs.href; 
+		console.log(`text ${text}, href ${href}`);
+		const t = text.split('-');
+		const yr = t[0];
+		const mon = t[1];
+		const day = t[2];
+		console.log(yr, mon, day);		
+		// let p = $(this).parent();
+		// console.log(p);
+	});
+})
+
+	//console.log(x);
+	//console.log(el);
+
+// for (var i=0; i<res2.length; i++) {
+// 	console.log(res2[i])
+// }
+
 // console.log(JSON.stringify(res2))
 
-for (var i=0; i<res1.tt.length; i++) {
-	console.log(res1.tt[i])
-	// console.log(res2.tt[i])	
-}
+// res2.forEach(function(element) {
+// 	console.log(JSON.stringify(element))
+// }, this);
+
+// for (var i=0; i<res1.tt.length; i++) {
+// 	console.log(res1.tt[i])
+// 	// console.log(res2.tt[i])	
+// }
 
 
