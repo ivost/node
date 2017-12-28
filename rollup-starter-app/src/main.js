bf4066733db3ecdb142ff105bb2406b330e98cb2
Ivo Stoyanov <ivostoy@gmail.com>
2017-12-28 13:31:45 -0800

    ok

--- rollup-starter-app/src/main.js
+++ rollup-starter-app/src/main.js
@@ -4,11 +4,13 @@ https://rollupjs.org/#npm-packages
 
 import update from './update.js';
 import answer from 'the-answer';
+import leftPad from 'left-pad';
 
 export default function () {
   console.log('the answer is ' + answer);
 }
 
+console.log("[" + leftPad('foo', 5) + "]");
 console.log('the answer is ' + answer);
 
 update();
